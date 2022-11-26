5a50126c40cb9147d371b431ebb0d7bfd078576e
Atik2788 <ashanto653.2@gmail.com>
2022-11-27 00:11:30 +0600

    after set of put status advertisement in bike status

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000
 
@@ -31,6 +31,9 @@ async function run() {
 
 
 
+    // ***************************  bikes  ***************************
+    // ***************************  bikes  ***************************
+
     // get bikes data from database
     app.get('/bikes', async (req, res) => {
       const query = {};
@@ -39,24 +42,59 @@ async function run() {
     })
 
     // post bike from add bike in database
-    app.post('/bikes', async(req, res)=>{
+    app.post('/bikes', async (req, res) => {
       const bike = req.body;
       const result = await bikesCollection.insertOne(bike)
       res.send(result)
     })
 
     // get bikes data by email
-    app.get('/bikes', async(req, res) =>{
-      const email = req.query.email;
-      const query = {email:email}
-      const result = await bikesCollection.find(query).toArray()
-      res.send(result)
+    app.get('/bikesemail', async (req, res) => {
+      const userEmail = req.query.email;
+      const query = { email: userEmail }
+      const bikes = await bikesCollection.find(query).toArray()
+      res.send(bikes)
     })
 
+    // post booking info in bike data
+    app.put('/bikes/:id', async(req, res)=>{
+
+      const id = req.params.id;
+      console.log(id);
+      const filter = {_id: ObjectId(id)}
+      const options = { upsert: true };
+      const updatedDoc = {
+          $set: {
+              status: 'booked',
+              // transactionId: payment.transactionId
+          }
+      }
+      const updatedResult = await bikesCollection.updateOne(filter, updatedDoc, options)
+
+      res.send(updatedResult)
+  })
+
+    // post booking info in bike data
+    app.put('/bikesAdvertise/:id', async(req, res)=>{
+
+      const id = req.params.id;
+      console.log(id);
+      const filter = {_id: ObjectId(id)}
+      const options = { upsert: true };
+      const updatedDoc = {
+          $set: {
+              status: 'advertised',
+              // transactionId: payment.transactionId
+          }
+      }
+      const updatedResult = await bikesCollection.updateOne(filter, updatedDoc, options)
+
+      res.send(updatedResult)
+  })
 
 
-
-
+    // ***************************  category  ***************************
+    // ***************************  category  ***************************
 
     // get category data from database
     app.get('/category', async (req, res) => {
@@ -74,17 +112,19 @@ async function run() {
     })
 
 
+    // ***************************  bookings  ***************************
+    // ***************************  bookings  ***************************
 
-    // get bookings data in database
-    app.get('/bookings', async(req, res)=>{
+    // get bookings data in database by email
+    app.get('/bookings', async (req, res) => {
       const email = req.query.email;
-      const query = {email:email}
+      const query = { email: email }
       const bookings = await bookingsCollection.find(query).toArray()
       res.send(bookings)
     })
 
     // post or add booking data in database
-    app.post('/bookings', async(req, res)=>{
+    app.post('/bookings', async (req, res) => {
       const booking = req.body;
       // console.log(booking)
       const result = await bookingsCollection.insertOne(booking)
@@ -93,7 +133,7 @@ async function run() {
 
 
     // post or add user information in database
-    app.post('/users', async(req, res)=>{
+    app.post('/users', async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user)
       res.send(result)
@@ -101,7 +141,7 @@ async function run() {
 
 
     // get question for blog from database
-    app.get('/blogs', async(req, res) =>{
+    app.get('/blogs', async (req, res) => {
       const query = {}
       const result = await blogsCollection.find(query).toArray()
       res.send(result)
